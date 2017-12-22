5b53675b94afb5a55116af36c5a465b2e918ce57
RoyPy <bogush.vano@gmail.com>
2017-12-22 16:56:46 +0700

    Rewrite using (now) symmetric methods of ProxyUnit Still incomplete, without parsing and connecting yet

--- src/ru/nsu/ccfit/bogush/net/http/proxy/Proxy.java
+++ src/ru/nsu/ccfit/bogush/net/http/proxy/Proxy.java
@@ -5,13 +5,13 @@ import ru.nsu.ccfit.bogush.net.http.HTTPParseException;
 import ru.nsu.ccfit.bogush.net.http.HTTPRequest;
 import ru.nsu.ccfit.bogush.net.http.HTTPResponse;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Objects;
@@ -25,6 +25,10 @@ public class Proxy {
         STATUS_LINES.put(501, new HTTPResponse().setStatusCode("501").setReasonPhrase("Not Implemented"));
         STATUS_LINES.put(502, new HTTPResponse().setStatusCode("502").setReasonPhrase("Bad Gateway"));
     }
+    private static final Charset US_ASCII = Charset.forName("US-ASCII");
+    private static final byte[] CRLF = "\r\n".getBytes(US_ASCII);
+    private static final byte CR = CRLF[0];
+    private static final byte LF = CRLF[1];
     private static int port = 50505;
     private static int bufferSize = 1 << 20;
     private static int backlog = 0;
@@ -63,79 +67,109 @@ public class Proxy {
         }
     }
 
-    private void loop()
-            throws IOException {
-        int selected;
-        do {
-            selected = selector.select();
-            System.err.println("Selector returned: " + selected);
-        } while (selected <= 0);
+    private void loop() throws IOException {
+        while (!Thread.interrupted()) {
+            int numberReady = selector.select();
+            while (numberReady <= 0) {
+                numberReady = selector.select();
+            }
 
-        Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-        while (iterator.hasNext()) {
-            SelectionKey key = iterator.next();
-
-            try {
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
                 if (!key.isValid()) {
+                    iterator.remove();
                     continue;
                 }
 
-                if (key.isAcceptable()) {
-                    accept(key);
-                    continue;
-                }
+                try {
+                    if (key.isAcceptable()) {
+                        accept(key);
+                        continue;
+                    }
 
-                if (key.isConnectable()) {
-                    connect(key);
-                    continue;
-                }
+                    if (key.isConnectable()) {
+                        connect(key);
+                        continue;
+                    }
 
-                if (key.isReadable()) {
-                    read(key);
-                }
+                    if (key.isReadable()) {
+                        int bytesRead = read(key);
+                        if (bytesRead == -1) continue;
+                    }
 
-                if (key.isWritable()) {
-                    write(key);
+                    if (key.isWritable()) {
+                        write(key);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    iterator.remove();
                 }
-            } finally {
-                iterator.remove();
             }
         }
     }
 
-    private void accept(SelectionKey key)
-            throws IOException {
-        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.attachment();
-        ProxyUnit unit = new ProxyUnit();
-        unit.local = serverSocketChannel.accept();
-        if (unit.local == null) return;
-        unit.local.configureBlocking(false);
-        unit.local.register(selector, OP_READ, unit);
+    private void accept(SelectionKey key) throws IOException {
+        ProxyUnit unit = new ProxyUnit(bufferSize);
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+        try {
+            unit.socket = serverSocketChannel.accept();
+            System.out.format("%-9s %s\n", "ACCEPT", unit);
+            unit.socket.configureBlocking(false);
+            unit.socket.register(selector, OP_READ, unit);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (unit.socket != null) {
+                try {
+                    unit.socket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            // TODO: 12/10/17 correctly handle exception
+            throw e;
+        }
     }
 
-    private static ProxyUnit attachmentAsUnit(SelectionKey key) {
-        return (ProxyUnit) key.attachment();
+    private void connect(SelectionKey key) throws IOException {
+        ProxyUnit unit = (ProxyUnit) key.attachment();
+        boolean connected;
+        try {
+            connected = unit.socket.finishConnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                unit.opposite.socket.close();
+                unit.socket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            throw e;
+        }
+
+        System.out.format("%-9s %s\n", "CONNECT", unit);
+        if (connected) {
+            removeOps(unit.socket.keyFor(selector), OP_CONNECT);
+            addOps(unit.socket.keyFor(selector), OP_READ | OP_WRITE);
+        }
     }
 
-    private void connect(SelectionKey key)
+    private int read(SelectionKey key)
             throws IOException {
-        ProxyUnit unit = attachmentAsUnit(key);
-        unit.finishConnect();
+        ProxyUnit unit = (ProxyUnit) key.attachment();
+        System.out.format("%-9s %s: ", "READ from", toString(unit.socket));
+        return unit.read();
     }
 
-    private void read(SelectionKey key)
+    private int write(SelectionKey key)
             throws IOException {
-        ProxyUnit unit = attachmentAsUnit(key);
-        boolean straight = key.channel() == unit.local;
-        unit.read(straight);
+        ProxyUnit unit = (ProxyUnit) key.attachment();
+        System.out.format("%-9s %s: ", "WRITE to", toString(unit.socket));
+        return unit.write();
     }
 
-    private void write(SelectionKey key)
-            throws IOException {
-        ProxyUnit unit = attachmentAsUnit(key);
-        boolean straight = key.channel() == unit.remote;
-        unit.write(straight);
-    }
 
     private void addOps(SelectionKey key, int ops) {
         key.interestOps(key.interestOps() | ops);
@@ -145,115 +179,165 @@ public class Proxy {
         key.interestOps(key.interestOps() & (~ops));
     }
 
+    private static String toString(SocketAddress address) {
+        if (address == null) return null;
+        InetSocketAddress addr = (InetSocketAddress) address;
+        String ip = addr.getAddress().getHostAddress();
+        int port = addr.getPort();
+        return String.format("%15s:%-5d", ip, port);
+    }
+
+    private static String toString(SelectableChannel s) {
+        String str = s.toString();
+        return str.substring(SocketChannel.class.getName().length());
+    }
+
+    private static String toString(SocketChannel channel) {
+        String local = null;
+        try {
+            local = toString(channel.getLocalAddress());
+        } catch (IOException ignored) { }
+        String remote = null;
+        try {
+            remote = toString(channel.getRemoteAddress());
+        } catch (IOException ignored) { }
+        return String.format("[%s - %s %s]", local, remote,
+                channel.isConnected() ? "connected" : channel.isConnectionPending() ? "connection pending" : "disconnected");
+    }
+
     private class ProxyUnit {
-        private SocketChannel local;
-        private SocketChannel remote;
-        private SocketChannel in;
-        private SocketChannel out;
-        private HTTPRequest request = new HTTPRequest(bufferSize);
-        private HTTPRequest response = new HTTPRequest(bufferSize);
-
         private ByteBuffer buf;
-        private boolean connected = false;
-        private boolean connecting = false;
+        private SocketChannel socket;
+        private ProxyUnit opposite;
+        private boolean eof = false;
+        private boolean outputIsShutdown = false;
+        private boolean httpMessageHeadParsed = false;
 
-        private void connect()
-                throws IOException {
-            if (!Objects.equals(request.getProtocol(), "http:")) {
-                local.shutdownInput();
-//                inverseBuf.put();
-            }
-            String host = request.getHost();
-            String portString = request.getPort();
-            if (portString == null) {
-                port = 80;
-            } else {
-                port = Integer.parseInt(portString);
-            }
-
-            InetSocketAddress address = new InetSocketAddress(host, port);
-            remote = local.provider().openSocketChannel();
-            remote.configureBlocking(false);
-            remote.register(selector, OP_CONNECT, this);
-            connecting = true;
-            connected = remote.connect(address);
+        private ProxyUnit(int bufferSize) {
+            this(null, bufferSize);
         }
 
-        private void finishConnect()
-                throws IOException {
-            connected = remote.finishConnect();
+        private ProxyUnit(SocketChannel socket, int bufferSize) {
+            this.socket = socket;
+            this.buf = ByteBuffer.allocate(bufferSize);
         }
 
-        private void configureDirection(boolean straight) {
-            in = straight ? local : remote;
-            out = straight ? remote : local;
-            buf = straight ? request.buffer() : response.buffer();
+        private void connect(InetSocketAddress address)
+                throws IOException {
+            opposite = new ProxyUnit(socket, bufferSize);
+            opposite.socket = SocketChannel.open();
+            opposite.socket.configureBlocking(false);
+            opposite.socket.connect(address);
+            opposite.opposite = this;
+            opposite.socket.register(selector, OP_CONNECT, opposite);
         }
 
-        private void read(boolean straight)
-                throws IOException {
-            configureDirection(straight);
-            buf.mark();
-            int bytesRead = in.read(buf);
-            if (!buf.hasRemaining()) {
-                removeOps(in.keyFor(selector), OP_READ);
-            }
+        private int read() throws IOException {
+            int bytesRead = socket.read(buf);
+
+            System.out.format("%d bytes\n", bytesRead);
+
             if (bytesRead > 0) {
-                if (straight) {
-                    if (!request.isHeadReady()) {
-                        try {
-                            int parseResult = request.parse();
-                            if (!connecting && (parseResult & HTTPRequest.URI) == HTTPRequest.URI) {
-                                connect();
-                            }
-                            if ((parseResult & HTTPRequest.FULL_REQUEST_HEAD) == HTTPRequest.FULL_REQUEST_HEAD) {
-                                request.setProtocol(null);
-                                request.setPort(null);
-                                request.setHost(null);
-                                request.setVersion("HTTP/1.0");
-                                request.setField("Connection", "close");
-                                request.build();
-                            }
-                        } catch (HTTPParseException e) {
-                            e.printStackTrace();
-                            // TODO send Bad Request
-                        }
-                    }
+                if (opposite.socket.isConnected()) {
+                    // opposite connected => it's a server response
+                    addOps(opposite.socket.keyFor(selector), OP_WRITE);
                 } else {
-                    if (!response.isHeadReady()) {
-                        try {
-                            int parseResult = response.parse();
-                            if ((parseResult & HTTPMessage.FULL_RESPONSE_HEAD) == HTTPRequest.FULL_RESPONSE_HEAD) {
-                                response.setVersion("HTTP/1.0");
-                                request.setField("Connection", "close");
-                                response.build();
-                            }
-                        } catch (HTTPParseException e) {
-                            e.printStackTrace();
-                            // TODO send Bad Request
-                        }
+                    // opposite not connected => it's a client request
+                    int emptyLineIndex = findEmptyLine(buf.array(), buf.position() - bytesRead, buf.position());
+                    if (emptyLineIndex > 0) {
+                        // request-head is between 0 and emptyLineIndex
+                        // parse request
+
                     }
                 }
             } else if (bytesRead == -1) {
-                // EOF from in
-                if (out.isOpen()) {
-                    out.shutdownInput();
+                // eof
+                eof = true;
+                removeOps(socket.keyFor(selector), OP_READ);
+
+                if (buf.position() == 0) {
+                    opposite.shutdownOutput();
+                    if (outputIsShutdown || opposite.buf.position() == 0) {
+                        close();
+                        opposite.close();
+                    }
                 }
-                in.keyFor(selector).cancel();
-                in.close();
-            } else {
-                System.err.format("read %d bytes - WTF?!\n\n", bytesRead);
+
             }
+
+            if (!buf.hasRemaining()) {
+                removeOps(socket.keyFor(selector), OP_READ);
+            }
+            return bytesRead;
         }
 
-        private void write(boolean straight)
-                throws IOException {
-            configureDirection(straight);
-            buf.flip();
-            int bytesWritten = out.write(buf);
-            if (bytesWritten > 0) {
-                buf.compact();
+        private int findEmptyLine(byte[] array, int from, int to) {
+            int cr = -3;
+            int lf = -3;
+            int pcr = cr-1;
+            for (int i = from; i < to; i++) {
+                byte b = array[i];
+                if (b == CR) {
+                    cr = i;
+                    pcr = cr;
+                } else if (b == LF) {
+                    if (cr == i-1 && lf == i-2) {
+                        return i - (pcr == i-3 ? 3 : 2);
+                    } else if (lf == i-1) {
+                        return i - (cr == i-2 ? 2 : 1);
+                    }
+                    lf = i;
+                }
             }
+
+            return -1;
+        }
+
+
+
+        private int write() throws IOException {
+            opposite.buf.flip();
+
+            int bytesWritten = socket.write(opposite.buf);
+
+            if (bytesWritten > 0) {
+                opposite.buf.compact();
+                addOps(opposite.socket.keyFor(selector), OP_READ);
+            }
+
+            System.out.format("%d bytes\n", bytesWritten);
+
+            if (opposite.buf.position() == 0) {
+                // wrote everything from opposite.buf
+                removeOps(socket.keyFor(selector), OP_WRITE);
+                if (opposite.eof) {
+                    shutdownOutput();
+                    if (opposite.outputIsShutdown) {
+                        close();
+                        opposite.close();
+                    }
+                }
+            }
+
+            return bytesWritten;
+        }
+
+        private void shutdownOutput()
+                throws IOException {
+            socket.shutdownOutput();
+            outputIsShutdown = true;
+        }
+
+        private void close()
+                throws IOException {
+            System.out.format("%-9s %s\n", "CLOSE", Proxy.toString(socket));
+            socket.close();
+            socket.keyFor(selector).cancel();
+        }
+
+        @Override
+        public String toString() {
+            return Proxy.toString(socket);
         }
     }
 }
